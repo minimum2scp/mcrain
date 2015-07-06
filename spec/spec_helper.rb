a2408a19359ed768f688dc2e9c330dd5009fe81f
akm <akm2000@gmail.com>
2015-07-06 18:14:57 +0900

    add DOCKER_HOST checking before run spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require "pry"
 
+unless ENV['DOCKER_HOST']
+  raise 'DOCKER_HOST is not exported. use `$(boot2docker shellinit)` or set DOCKER_HOST like tcp://127.0.0.1:2375'
+end
+
 if ENV["COVERAGE"] =~ /true|yes|on|1/i
   require "simplecov"
   SimpleCov.start :rails
