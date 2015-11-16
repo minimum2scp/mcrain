7a73da39214d70bd7e767ceaaef65131d8bd53ce
akm <akm2000@gmail.com>
2015-11-16 14:03:24 +0900

    fix not to use Mcrain[...]

--- spec/mcrain/mysql_spec.rb
+++ spec/mcrain/mysql_spec.rb
@@ -7,7 +7,7 @@ describe Mcrain::Mysql do
 
   context ".start" do
     it "ping" do
-      Mcrain[:mysql].start do |s|
+      Mcrain::Mysql.new.start do |s|
         expect(s.client.query("show databases").map(&:values).flatten.sort).to eq ["information_schema", "mysql", "performance_schema"].sort
       end
     end
@@ -20,7 +20,6 @@ describe Mcrain::Mysql do
         end
       end
       let(:tmp_db_dir){ @tmp_db_dir }
-      after { Mcrain[:mysql].db_dir = nil }
 
       let(:dbname){ "testdb1" }
       let(:tablename){ "testtable1" }
@@ -28,8 +27,7 @@ describe Mcrain::Mysql do
 
       it do
         old_cid = nil
-        Mcrain[:mysql].db_dir = tmp_db_dir
-        Mcrain[:mysql].start do |s|
+        Mcrain::Mysql.new(db_dir: tmp_db_dir).start do |s|
           old_cid = s.container.id
           c0 = s.build_client
           c0.query("CREATE DATABASE #{dbname}")
@@ -37,8 +35,7 @@ describe Mcrain::Mysql do
           c0.query("CREATE TABLE #{tablename} (id integer NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL);")
           c0.query("INSERT INTO #{tablename} (name) VALUES ('#{tabledata1}');")
         end
-        Mcrain[:mysql].db_dir = tmp_db_dir
-        Mcrain[:mysql].start do |s|
+        Mcrain::Mysql.new(db_dir: tmp_db_dir).start do |s|
           expect(s.container.id).to_not eq old_cid
           c1 = s.build_client
           c1.select_db(dbname)
@@ -52,34 +49,32 @@ describe Mcrain::Mysql do
   context "start twice" do
     it do
       first = nil
-      Mcrain[:mysql].start do |s|
+      Mcrain::Mysql.new.start do |s|
         first = s.client
         expect(s.client).to eq first
       end
-      Mcrain[:mysql].start do |s|
+      Mcrain::Mysql.new.start do |s|
         expect(s.client).to_not eq first
       end
     end
   end
 
   context "skip_reset_after_teardown" do
-    after{ Mcrain[:mysql].skip_reset_after_teardown = nil }
-
     it false do
-      Mcrain[:mysql].skip_reset_after_teardown = false
-      first_url = Mcrain[:mysql].url
-      Mcrain[:mysql].start{ }
-      expect(Mcrain[:mysql].url).to_not eq first_url
+      s = Mcrain::Mysql.new(skip_reset_after_teardown: false)
+      first_url = s.url
+      s.start{ }
+      expect(s.url).to_not eq first_url
     end
 
     it true do
-      Mcrain[:mysql].skip_reset_after_teardown = true
+      s = Mcrain::Mysql.new(skip_reset_after_teardown: true)
       begin
-        first_url = Mcrain[:mysql].url
-        Mcrain[:mysql].start{ }
-        expect(Mcrain[:mysql].url).to eq first_url
+        first_url = s.url
+        s.start{ }
+        expect(s.url).to eq first_url
       ensure
-        Mcrain[:mysql].reset
+        s.reset
       end
     end
   end
