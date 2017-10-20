3a3f168f7595067870aacbf501796424acde70ce
YAMADA Tsuyoshi <tsu-yamada@groovenauts.jp>
2017-10-20 13:37:13 +0900

    Added test for Mcrain.load_config

--- spec/mcrain/configuration_spec.rb
+++ spec/mcrain/configuration_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "tempfile"
 
 describe Mcrain do
   describe ".configuration" do
@@ -8,23 +9,23 @@ describe Mcrain do
     end
 
     it "is default mysql image" do
-      expect(Mcrain::Mysql.container_image).to eq Mcrain::Configuration::DEFAULT_IMAGES[:mysql]
+      expect(Mcrain::Mysql.container_image).to     eq Mcrain::Configuration::DEFAULT_IMAGES[:mysql]
     end
 
     it "is default redis image" do
-      expect(Mcrain::Redis.container_image).to eq Mcrain::Configuration::DEFAULT_IMAGES[:redis]
+      expect(Mcrain::Redis.container_image).to     eq Mcrain::Configuration::DEFAULT_IMAGES[:redis]
     end
 
     it "is default rabbitmq image" do
-      expect(Mcrain::Rabbitmq.container_image).to eq Mcrain::Configuration::DEFAULT_IMAGES[:rabbitmq]
+      expect(Mcrain::Rabbitmq.container_image).to  eq Mcrain::Configuration::DEFAULT_IMAGES[:rabbitmq]
     end
 
     it "is default riak image" do
-      expect(Mcrain::Riak.container_image).to eq Mcrain::Configuration::DEFAULT_IMAGES[:riak]
+      expect(Mcrain::Riak.container_image).to      eq Mcrain::Configuration::DEFAULT_IMAGES[:riak]
     end
 
     it "is default hbase image" do
-      expect(Mcrain::Hbase.container_image).to eq Mcrain::Configuration::DEFAULT_IMAGES[:hbase]
+      expect(Mcrain::Hbase.container_image).to     eq Mcrain::Configuration::DEFAULT_IMAGES[:hbase]
     end
   end
 
@@ -45,23 +46,71 @@ describe Mcrain do
     end
 
     it "is configured mysql image" do
-      expect(Mcrain::Mysql.container_image).to eq "customized/mysql:x.x"
+      expect(Mcrain::Mysql.container_image).to     eq "customized/mysql:x.x"
     end
 
     it "is configured redis image" do
-      expect(Mcrain::Redis.container_image).to eq "customized/redis:x.x"
+      expect(Mcrain::Redis.container_image).to     eq "customized/redis:x.x"
     end
 
     it "is configured rabbitmq image" do
-      expect(Mcrain::Rabbitmq.container_image).to eq "customized/rabbitmq:x.x"
+      expect(Mcrain::Rabbitmq.container_image).to  eq "customized/rabbitmq:x.x"
     end
 
     it "is configured riak image" do
-      expect(Mcrain::Riak.container_image).to eq "customized/riak:x.x"
+      expect(Mcrain::Riak.container_image).to      eq "customized/riak:x.x"
     end
 
     it "is configured hbase image" do
-      expect(Mcrain::Hbase.container_image).to eq "customized/hbase:x.x"
+      expect(Mcrain::Hbase.container_image).to     eq "customized/hbase:x.x"
+    end
+  end
+
+  describe ".load_config" do
+    before do
+      config_content = <<~CONFIG
+          ---
+          images:
+            mysql: "another/mysql:x.x"
+            redis: "another/redis:x.x"
+            rabbitmq: "another/rabbitmq:x.x"
+            riak: "another/riak:x.x"
+            hbase: "another/hbase:x.x"
+      CONFIG
+
+      @config_file = Tempfile.open(["mcrain-test-config", ".yml"]) do |fh|
+        fh << config_content
+      end
+    end
+
+    after do
+      # reset configuration
+      Mcrain.configuration = Mcrain::Configuration.new
+    end
+
+    it "is configured mysql image" do
+      Mcrain.load_config @config_file
+      expect(Mcrain::Mysql.container_image).to     eq "another/mysql:x.x"
+    end
+
+    it "is configured redis image" do
+      Mcrain.load_config @config_file
+      expect(Mcrain::Redis.container_image).to     eq "another/redis:x.x"
+    end
+
+    it "is configured rabbitmq image" do
+      Mcrain.load_config @config_file
+      expect(Mcrain::Rabbitmq.container_image).to  eq "another/rabbitmq:x.x"
+    end
+
+    it "is configured riak image" do
+      Mcrain.load_config @config_file
+      expect(Mcrain::Riak.container_image).to      eq "another/riak:x.x"
+    end
+
+    it "is configured hbase image" do
+      Mcrain.load_config @config_file
+      expect(Mcrain::Hbase.container_image).to     eq "another/hbase:x.x"
     end
   end
 end
